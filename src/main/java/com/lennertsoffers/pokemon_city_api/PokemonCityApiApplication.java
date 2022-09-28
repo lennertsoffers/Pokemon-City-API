852c53fb93fc11508a936e377757df2168245d0f
lennertsoffers <lennert.soffers@hotmail.com>
2022-09-28 11:34:49 +0300

    Create Project

---
+++ src/main/java/com/lennertsoffers/pokemon_city_api/PokemonCityApiApplication.java
@@ -0,0 +1,13 @@
+package com.lennertsoffers.pokemon_city_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonCityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lennertsoffers/pokemon_city_api/PokemonCityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lennertsoffers.pokemon_city_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonCityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
