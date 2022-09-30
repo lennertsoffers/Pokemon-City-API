52d9bf1f3d1d6a7a90430f26654d559d9293cf5c
lennertsoffers <lennert.soffers@hotmail.com>
2022-09-30 14:48:30 +0300

    Validation for building

--- src/main/java/com/lennertsoffers/pokemon_city_api/model/dto/BuildableBuildDto.java
+++ src/main/java/com/lennertsoffers/pokemon_city_api/model/dto/BuildableBuildDto.java
@@ -1,10 +1,12 @@
 package com.lennertsoffers.pokemon_city_api.model.dto;
 
+import com.lennertsoffers.pokemon_city_api.validation.BuildableBuildConstraint;
 import com.lennertsoffers.pokemon_city_api.validation.BuildableTypeConstraint;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@BuildableBuildConstraint
 public record BuildableBuildDto(
         @NotBlank String name,
         @NotNull int x,
