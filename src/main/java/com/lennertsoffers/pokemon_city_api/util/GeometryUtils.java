f25d3c0fce2c7f8bafe9c7b8fbeec158ad811a9a
lennertsoffers <lennert.soffers@hotmail.com>
2022-10-18 10:51:25 +0300

    Working GeometryUtils

--- src/main/java/com/lennertsoffers/pokemon_city_api/util/GeometryUtils.java
+++ src/main/java/com/lennertsoffers/pokemon_city_api/util/GeometryUtils.java
@@ -4,20 +4,22 @@ import com.lennertsoffers.pokemon_city_api.model.Buildable;
 
 public class GeometryUtils {
     public static boolean collidesWith(Buildable buildable, int x, int y, int width, int height) {
-        int a_x1 = buildable.getLocation().getX() - 1;
-        int a_x2 = buildable.getLocation().getX() + buildable.getWidth();
-        int a_y1 = buildable.getLocation().getY() - 1;
-        int a_y2 = buildable.getLocation().getY() + buildable.getHeight();
+        int a_x1 = buildable.getLocation().getX() - buildable.getWidth() + 1;
+        int a_x2 = buildable.getLocation().getX();
+        int a_y1 = buildable.getLocation().getY() - buildable.getHeight() + 1;
+        int a_y2 = buildable.getLocation().getY();
 
-        int b_x2 = x + width - 1;
-        int b_y2 = y + height - 1;
+        int b_x1 = x - width + 1;
+        int b_y1 = y - height + 1;
 
-        boolean ax1 = a_x1 >= x && a_x1 <= b_x2;
-        boolean ax2 = a_x2 >= x && a_x2 <= b_x2;
+        boolean x1Collision = a_x1 <= b_x1 && a_x2 >= b_x1;
+        boolean x2Collision = a_x1 <= x && a_x2 >= x;
+        boolean xCollision = x1Collision || x2Collision;
 
-        boolean ay1 = a_y1 >= y && a_y1 <= b_y2;
-        boolean ay2 = a_y2 >= y && a_y2 <= b_y2;
+        boolean y1Collision = a_y1 <= b_y1 && a_y2 >= b_y1;
+        boolean y2Collision = a_y1 <= y && a_y2 >= y;
+        boolean yCollision = y1Collision || y2Collision;
 
-        return (ax1 || ax2) && (ay1 || ay2);
+        return xCollision && yCollision;
     }
 }
